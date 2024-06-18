58968c2f73ee92eea064ed6a7370370fefb82728
xiaoyaocz <xiaoyaocz@52uwp.com>
2024-06-18 14:20:22 +0800

    更新控制台程序

--- AllLive.Console/Program.cs
+++ AllLive.Console/Program.cs
@@ -4,7 +4,6 @@ using AllLive.Core.Helper;
 using AllLive.Core.Interface;
 using AllLive.UWP.Helper;
 using Newtonsoft.Json;
-using QuickJS;
 using System;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -15,113 +14,162 @@ namespace AllLive.ConsoleApp
     {
         static async Task Main(string[] args)
         {
+            do
+            {
+                Console.Clear();
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("请选择要执行的操作：");
+                    Console.WriteLine("【1】获取直播间直链");
+                    Console.WriteLine("【2】实时输出弹幕");
+                    Console.WriteLine("【3】退出程序");
+                    Console.Write("操作序号：");
+                    var input = Console.ReadLine();
+
+                    if (input == "3")
+                    {
+                        break;
+                    }
+
+                    if (input != "1" && input != "2")
+                    {
+                        continue;
+                    }
+                    Console.Clear();
+                    Console.WriteLine("请输入直播间链接，支持哔哩哔哩、斗鱼直播、虎牙直播、抖音直播");
+                    Console.Write("URL：");
+                    var url = Console.ReadLine();
+                    args = new string[] { input == "1" ? "-i" : "-d", url };
+                    Console.Clear();
+                }
+                try
+                {
+                    await Action(args);
+                    args = new string[0];
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                    break;
+                }
+
+
+
+            } while (true);
+
+        }
+
+        private static async Task Action(string[] args)
+        {
             if (args.Length < 1)
             {
-                Console.WriteLine("输入错误");
+                Console.WriteLine("参数有误,详细参数信息请通过-h查看");
                 return;
             }
-           
+
+
             var action = args[0];
-            
-            try
+
+            if (args.Length == 1)
             {
-                if (args.Length==1)
+                if (action == "h" || action == "-h" || action == "?")
                 {
-                    if(action == "h" || action == "-h" || action == "?")
-                    {
-                        Console.WriteLine("-i [URL] ：获取直播间信息及播放直链");
-                        Console.WriteLine("-d [URL] ：持续输出直播间弹幕");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"未知指令：{action}");
-                    }
-                    return;
-                }
-                var url = args[1];
-                var parseData =  await SiteParser.ParseUrl(url);
-                ILiveSite site=null;
-                switch (parseData.Item1)
-                {
-                    case LiveSite.Bilibili:
-                        site = new BiliBili();
-                        break;
-                    case LiveSite.Douyu:
-                        site = new Douyu();
-                        break;
-                    case LiveSite.Huya:
-                        site = new Huya();
-                        break;
-                    case LiveSite.Douyin:
-                        site = new Douyin();
-                        break;
-                    case LiveSite.Unknown:
-                        Console.WriteLine("未知直播源");
-                        return;
-                    default:
-                        break;
-                }
-
-                var roomId = parseData.Item2;
-                if (action == "i"|| action == "-i")
-                {
-                    var detail = site.GetRoomDetail(roomId).Result;
-                    Console.WriteLine($"来源：{site.Name}");
-                    Console.WriteLine($"房间号：{detail.RoomID}");
-                    Console.WriteLine($"房间标题：{detail.Title}");
-                    Console.WriteLine($"直播用户：{detail.UserName}");
-                    Console.WriteLine($"人气值：{detail.Online}");
-                    Console.WriteLine($"状态：{(detail.Status?"直播中":"未开播")}");
-                    if (detail.Status)
-                    {
-                        Console.WriteLine($"可用清晰度：");
-                        var quality = site.GetPlayQuality(detail).Result;
-
-                        for (int i = 0; i < quality.Count; i++)
-                        {
-                            Console.WriteLine($"【{i}】{quality[i].Quality}");
-                        }
-                        Console.WriteLine($"请输入【】内数字，获取对应清晰度的直链");
-                        var input = Console.ReadLine();
-                        Console.WriteLine($"正在获取直链...");
-                        if (int.TryParse(input, out var index))
-                        {
-                            var urls = site.GetPlayUrls(detail, quality[index]).Result;
-                            for (int i = 0; i < urls.Count; i++)
-                            {
-                                Console.WriteLine($"线路{i + 1}:\r\n{urls[i]}");
-                            }
-                        }
-                    }
-                    
-                }
-                else if (action == "d"||action == "-d")
-                {
-                    Console.WriteLine($"正在获取房间信息...");
-                    var detail = site.GetRoomDetail(roomId).Result;
-                    Console.WriteLine($"房间标题：{detail.Title}");
-                    Console.WriteLine($"直播用户：{detail.UserName}");
-                    var danmaku = site.GetDanmaku();
-                    danmaku.NewMessage += Danmaku_NewMessage;
-                    danmaku.OnClose += Danmaku_OnClose;
-                    Console.WriteLine($"【开始获取弹幕，按任意键结束】");
-                    danmaku.Start(detail.DanmakuData).Wait();
-                    Console.ReadLine();
+                    Console.WriteLine("-i [URL] ：获取直播间信息及播放直链");
+                    Console.WriteLine("-d [URL] ：持续输出直播间弹幕");
                 }
                 else
                 {
                     Console.WriteLine($"未知指令：{action}");
                 }
+                return;
             }
-            catch (Exception ex)
+            var url = args[1];
+            var parseData = await SiteParser.ParseUrl(url);
+            ILiveSite site = null;
+            switch (parseData.Item1)
             {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.StackTrace);
+                case LiveSite.Bilibili:
+                    site = new BiliBili();
+                    break;
+                case LiveSite.Douyu:
+                    site = new Douyu();
+                    break;
+                case LiveSite.Huya:
+                    site = new Huya();
+                    break;
+                case LiveSite.Douyin:
+                    site = new Douyin();
+                    break;
+                case LiveSite.Unknown:
+                    Console.WriteLine("未知直播源");
+                    return;
+                default:
+                    break;
             }
 
+            var roomId = parseData.Item2;
+            if (action == "i" || action == "-i")
+            {
+                Console.WriteLine($"正在获取房间信息...");
+                var detail = await site.GetRoomDetail(roomId);
+                Console.WriteLine($"来源：{site.Name}");
+                Console.WriteLine($"房间号：{detail.RoomID}");
+                Console.WriteLine($"房间标题：{detail.Title}");
+                Console.WriteLine($"直播用户：{detail.UserName}");
+                Console.WriteLine($"人气值：{detail.Online}");
+                Console.Write("状态：");
+                Console.ForegroundColor = detail.Status ? ConsoleColor.Green : ConsoleColor.Red;
+                Console.WriteLine(detail.Status ? "直播中" : "未开播");
+                Console.ResetColor();
+                if (detail.Status)
+                {
+                    Console.WriteLine($"可用清晰度：");
+                    var quality = await site.GetPlayQuality(detail);
+
+                    for (int i = 0; i < quality.Count; i++)
+                    {
+                        Console.WriteLine($"【{i}】{quality[i].Quality}");
+                    }
+                    Console.Write($"请输入清晰度序号：");
+                    var input = Console.ReadLine();
+                    Console.WriteLine($"正在获取直链...");
+                    if (int.TryParse(input, out var index))
+                    {
+                        var urls = await site.GetPlayUrls(detail, quality[index]);
+                        for (int i = 0; i < urls.Count; i++)
+                        {
+                            Console.WriteLine($"线路{i + 1}：{urls[i]}");
+                        }
+                    }
+                    Console.WriteLine($"按任意键结束");
+                    Console.ReadKey();
+                }
+
+            }
+            else if (action == "d" || action == "-d")
+            {
+                Console.WriteLine($"正在获取房间信息...");
+                var detail = await site.GetRoomDetail(roomId);
+                Console.WriteLine($"房间标题：{detail.Title}");
+                Console.WriteLine($"直播用户：{detail.UserName}");
+                Console.Write("直播状态：");
+                Console.ForegroundColor = detail.Status ? ConsoleColor.Green : ConsoleColor.Red;
+                Console.WriteLine(detail.Status ? "直播中" : "未开播");
+                Console.ResetColor();
+                var danmaku = site.GetDanmaku();
+                danmaku.NewMessage += Danmaku_NewMessage;
+                danmaku.OnClose += Danmaku_OnClose;
+                Console.WriteLine($"【开始获取弹幕，按任意键结束】");
+                danmaku.Start(detail.DanmakuData).Wait();
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"未知指令：{action}");
+            }
         }
 
-       
 
         private static void Danmaku_OnClose(object sender, string e)
         {
@@ -132,8 +180,8 @@ namespace AllLive.ConsoleApp
         {
             if (e.Type == Core.Models.LiveMessageType.Chat)
             {
-                Console.ForegroundColor = ClosestConsoleColor(e.Color.R,e.Color.G,e.Color.B);
-                Console.WriteLine($"[{e.Color.R.ToString("X2")}{e.Color.G.ToString("X2")}{e.Color.B.ToString("X2")}][{e.UserName}]：{e.Message}");
+                Console.ForegroundColor = ClosestConsoleColor(e.Color.R, e.Color.G, e.Color.B);
+                Console.WriteLine($"[{e.UserName}]：{e.Message}");
                 Console.ResetColor();
             }
             if (e.Type == Core.Models.LiveMessageType.Online)
