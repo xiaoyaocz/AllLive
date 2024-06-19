6f2557134af403359d84e3c7cf6d08782406d91c
xiaoyaocz <xiaoyaocz@52uwp.com>
2024-06-19 11:27:28 +0800

    支持导入、导出关注列表

--- AllLive.UWP/ViewModels/FavoriteVM.cs
+++ AllLive.UWP/ViewModels/FavoriteVM.cs
@@ -6,6 +6,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Newtonsoft.Json;
+using Windows.UI.Popups;
 
 namespace AllLive.UWP.ViewModels
 {
@@ -14,7 +19,15 @@ namespace AllLive.UWP.ViewModels
         public FavoriteVM()
         {
             Items = new ObservableCollection<FavoriteItem>();
+            InputCommand = new RelayCommand(Input);
+            OutputCommand = new RelayCommand(Output);
+            TipCommand = new RelayCommand(Tip);
         }
+
+        public ICommand InputCommand { get; set; }
+        public ICommand OutputCommand { get; set; }
+        public ICommand TipCommand { get; set; }
+
 
         private ObservableCollection<FavoriteItem> _items;
         public ObservableCollection<FavoriteItem> Items
@@ -93,7 +106,7 @@ namespace AllLive.UWP.ViewModels
                     Items = new ObservableCollection<FavoriteItem>(Items.OrderByDescending(x => x.LiveStatus));
                 }
             }
-        } 
+        }
 
 
         public override void Refresh()
@@ -118,6 +131,150 @@ namespace AllLive.UWP.ViewModels
 
         }
 
+        public async void Input()
+        {
+          
+            // 打开文件选择器
+            FileOpenPicker picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".json");
+            picker.SuggestedStartLocation = PickerLocationId.Desktop;
+            picker.ViewMode = PickerViewMode.List;
+            picker.CommitButtonText = "导入";
 
+            var file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                try
+                {
+                    var json = await FileIO.ReadTextAsync(file);
+                    var items = Newtonsoft.Json.JsonConvert.DeserializeObject<List<FavoriteJsonItem>>(json);
+                    foreach (var item in items)
+                    {
+
+                        DatabaseHelper.AddFavorite(new FavoriteItem()
+                        {
+                            SiteName = item.SiteName,
+                            RoomID = item.RoomId,
+                            UserName = item.UserName,
+                            Photo = item.Face,
+                        });
+                    }
+                    Utils.ShowMessageToast("导入成功");
+                    Refresh();
+                }
+                catch (Exception ex)
+                {
+                    HandleError(ex);
+                    Utils.ShowMessageToast("导入失败");
+                }
+            }
+        }
+
+        public async void Output()
+        {
+            // 打开文件选择器
+            FileSavePicker picker = new FileSavePicker();
+            picker.FileTypeChoices.Add("Json", new List<string>() { ".json" });
+            picker.SuggestedStartLocation = PickerLocationId.Desktop;
+            picker.SuggestedFileName = "favorite.json";
+
+            var file = await picker.PickSaveFileAsync();
+            if (file != null)
+            {
+                try
+                {
+                    var items = new List<FavoriteJsonItem>();
+                    foreach (var item in Items)
+                    {
+                        var siteId = "";
+                        switch(item.SiteName)
+                        {
+                            case "哔哩哔哩直播":
+                                siteId = "bilibili";
+                                break;
+                            case "斗鱼直播":
+                                siteId = "douyu";
+                                break;
+                            case "虎牙直播":
+                                siteId = "huya";
+                                break;
+                            case "抖音直播":
+                                siteId = "douyin";
+                                break;
+                        }
+
+                        items.Add(new FavoriteJsonItem()
+                        {
+                            SiteId = siteId,
+                            Id = $"{siteId}_{item.RoomID}",
+                            RoomId = item.RoomID,
+                            UserName = item.UserName,
+                            Face = item.Photo,
+                            AddTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.M")
+                        });
+                    }
+                    var json = Newtonsoft.Json.JsonConvert.SerializeObject(items);
+                    await FileIO.WriteTextAsync(file, json);
+                    Utils.ShowMessageToast("导出成功");
+                }
+                catch (Exception ex)
+                {
+                    HandleError(ex);
+                    Utils.ShowMessageToast("导出失败");
+                }
+            }
+
+
+        }
+
+        public void Tip()
+        {
+            MessageDialog dialog = new MessageDialog(@"该程序兼容Simple Live，您可以导入Simple Live的关注数据，导出的数据也可以在Simple Live中导入。
+由于UWP限制，无法实现与Simple Live数据同步，请手动导出、导出数据。", "导入导出说明");
+           _= dialog.ShowAsync();
+        }
+    }
+
+    public class FavoriteJsonItem
+    {
+        [JsonProperty("siteId")]
+        public string SiteId;
+
+        [JsonProperty("id")]
+        public string Id;
+
+        [JsonProperty("roomId")]
+        public string RoomId;
+
+        [JsonProperty("userName")]
+        public string UserName;
+
+        [JsonProperty("face")]
+        public string Face;
+
+        [JsonProperty("addTime")]
+        public string AddTime;
+
+        [JsonIgnore]
+        public string SiteName
+        {
+            get
+            {
+                switch (SiteId)
+                {
+                    case "bilibili":
+                        return "哔哩哔哩直播";
+                    case "douyu":
+                        return "斗鱼直播";
+                    case "huya":
+                        return "虎牙直播";
+                    case "douyin":
+                        return "抖音直播";
+                    default:
+                        return "未知";
+                }
+            }
+        }
+        
     }
 }
